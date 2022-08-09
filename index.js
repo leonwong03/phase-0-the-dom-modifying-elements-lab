4fb8f97f484b3a3ec7e8dcdc6dc8ae1a8d17c7b2
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-09 13:36:28 -0400

    commit tree a9f20bc923312046421827d79ed9740378597ed5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+
+const newHeader = document.createElement("H1");
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Urmom is the champion";
+
+document.body.append(newHeader);
